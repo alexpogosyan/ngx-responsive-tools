3b23247fa458ce0ad596faf4ce3926565969c7d3
Alex Pogosyan <alexpogosyan@gmail.com>
2024-02-18 18:16:08 +0400

    Add responsive service

--- src/lib/ngx-responsive-tools.service.ts
+++ src/lib/ngx-responsive-tools.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NgxResponsiveToolsService {
+  xs$ = this.breakpointObserver
+    .observe("(max-width: 479px)")
+    .pipe(map((bp: BreakpointState) => bp?.matches));
 
-  constructor() { }
+  sm$ = this.breakpointObserver
+    .observe("(min-width: 480px)")
+    .pipe(map((bp: BreakpointState) => bp?.matches));
+
+  md$ = this.breakpointObserver
+    .observe("(min-width: 768px)")
+    .pipe(map((bp: BreakpointState) => bp?.matches));
+
+  lg$ = this.breakpointObserver
+    .observe("(min-width: 1024px)")
+    .pipe(map((bp: BreakpointState) => bp?.matches));
+
+  xl$ = this.breakpointObserver
+    .observe("(min-width: 1200px)")
+    .pipe(map((bp: BreakpointState) => bp?.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
